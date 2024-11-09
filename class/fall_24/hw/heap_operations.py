cd98203cb53133941122506119b4247d5ce73de0
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-11-09 18:37:16 -0500

    updating notes

--- class/fall_24/hw/heap_operations.py
+++ class/fall_24/hw/heap_operations.py
@@ -25,11 +25,18 @@ class HeapFactory(ABC):
 
     # @abstractmethod
     def build_heap(self):
+        """Take an unordered list of n elements. 
+        O(log n): Call min_heapify() which worse case traverses the height of the heap log_2 (n) to restore heap property
+        O(n): For every element, we call min_heapify()
+        """
         pass 
 
     # @abstractmethod
     def min_heapify(self, i: int):
         """To maintain the min-heap property of the parent node being smaller than the child node. If the parent node is larger than either of its children, the function "sinks" the parent down the tree, swapping it with the smallest child, until the heap property is restored.
+
+        O(log n): Call min_heapify() which worse case traverses the height of the heap log_2 (n) to restore heap property
+        O(i=1): For element i, call min_heapify()
         """
     
         l = self.get_left_node(i)
@@ -61,6 +68,25 @@ class SeatHeap(HeapFactory):
             self.min_heapify(node)
         
         return self.network
+    
+    def extract_min(self):
+        """
+        Example with self.network = [1, 2, 3, 4, 5]
+            self.network_size = 5
+        """
+        if self.network_size < 1:
+            return "Heap underflow"
+        
+        min_node = self.network[0] # Min node is root node, so 1
+        self.network[0] = self.network[-1] # Replace root node with last node to maintain the complete binary tree structure, so [5, 2, 3, 4, 5]
+        self.network.pop() # Remove last element, so [5, 2, 3, 4]
+        self.network_size = len(self.network) # Update size, so 4
+        self.min_heapify(0)  # self.network = [2, 3, 4, 5]
+
+        return min_node
 
 class WaitlistHeap(HeapFactory):
     """Store users in waitlist"""
+
+    def build_heap(self):
+        return super().build_heap()
