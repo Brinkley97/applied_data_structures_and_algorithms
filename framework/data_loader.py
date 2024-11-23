a093c13ec5d8e22195b868e97328ffbc4f706fdf
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-11-22 22:20:06 -0500

    load dict with graph-tool

--- framework/data_loader.py
+++ framework/data_loader.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import graph_tool.all as gt
 
 from abc import ABC, abstractmethod
 
@@ -17,3 +18,35 @@ class LoadNetworkX(LoadNetworkFactory):
     def dict_to_graph(self, network):
         return nx.to_networkx_graph(network)
     
+class LoadPyG(LoadNetworkFactory):
+    """A class that inherits from LoadNetworkFactory with the utilization of PyG"""
+
+    def dict_to_graph(self):
+        pass
+
+class LoadGraphTool(LoadNetworkFactory):
+    """A class that inherits from LoadNetworkFactory with the utilization of graph-tool"""
+
+    def dict_to_graph(self, network: dict, directed: bool, all_integers: bool):
+        g = gt.Graph(directed=directed)
+
+        if all_integers:
+            # Add nodes
+            for node in network:
+                g.add_vertex(node + 1)  # Ensure all nodes are added
+
+            # Add edges
+            for from_node, to_nodes in network.items():
+                for to_node in to_nodes:
+                    g.add_edge(from_node, to_node)
+        else:
+            # Prepare edge list
+            edge_list = []
+            for from_node, to_nodes in network.items():
+                for to_node in to_nodes:
+                    edge_list.append((from_node, to_node))
+            
+            # Add edges with hashed=True
+            g.add_edge_list(edge_list, hashed=True)
+
+        return g
